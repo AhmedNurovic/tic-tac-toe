499b82f8d20030f795b1817c43f436ef8c2b4c6d
Ahmed Nurović <ahmed.nurovic@hotmail.com>
2020-10-20 16:38:53 +0200

    Updates

---
+++ precache-manifest.9c7cf9fd1f03e0d7b5b9a3034cfce010.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1b17cd20ee61b32007c56d85a2eb1a1",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "c38f47d5fd550f2eff6e",
+    "url": "/tic-tac-toe/static/css/main.474fc7eb.chunk.css"
+  },
+  {
+    "revision": "2fde1f830cbaae1ad9f4",
+    "url": "/tic-tac-toe/static/js/2.7306dc82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.7306dc82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c38f47d5fd550f2eff6e",
+    "url": "/tic-tac-toe/static/js/main.089b41c2.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
